2d58bfbebaf150abdd77a63ee89f66cbfeb7a510
Leonard Göhrs <l.goehrs@pengutronix.de>
2023-09-04 15:21:05 +0200

    Check sanity-check ipv6 addresses vs. MACs

--- tests/test_userspace.py
+++ tests/test_userspace.py
@@ -61,9 +61,39 @@ def test_switch_configuration(shell):
         if b["ifname"] == uplink_ifname:
             assert b["state"] == "forwarding"
 
-    [ip] = shell.run_check("ip -d -j link show tac-bridge")
-    [ip_json] = json.loads(ip)
+    [ip_link] = shell.run_check("ip -d -j link show tac-bridge")
+    [ip_link_json] = json.loads(ip_link)
 
     # link attributes are in expected state
-    assert ip_json["linkinfo"]["info_data"]["stp_state"] == 0
-    assert ip_json["linkinfo"]["info_data"]["mcast_snooping"] == 0
+    assert ip_link_json["linkinfo"]["info_data"]["stp_state"] == 0
+    assert ip_link_json["linkinfo"]["info_data"]["mcast_snooping"] == 0
+
+    [ip_addr] = shell.run_check("ip -d -j addr show tac-bridge")
+    [ip_addr_json] = json.loads(ip_addr)
+
+    mac = ip_addr_json["address"]
+    link_local_v6 = next(
+        ai["local"]
+        for ai in ip_addr_json["addr_info"]
+        if ai["family"] == "inet6" and ai['scope'] == "link"
+    )
+    global_v6 = next(
+        ai["local"]
+        for ai in ip_addr_json["addr_info"]
+        if ai["family"] == "inet6" and ai['scope'] == "global"
+    )
+
+    # Each TAC is assigned 16 MAC addresses and the tac-bridge should use
+    # the one ending in '3'.
+    assert mac[-1] == '3'
+
+    # Check if the configured v6 addresses are derived from the current MAC address.
+    v6_tail = mac.split(":")
+    v6_tail.insert(3,"fe")
+    v6_tail.insert(3,"ff")
+    v6_tail = list(int(a + b, 16) for a, b in zip(v6_tail[::2], v6_tail[1::2]))
+    v6_tail[0] ^= 0x0200
+    v6_tail = ":".join(f"{a:x}" for a in v6_tail)
+
+    assert link_local_v6.endswith(v6_tail)
+    assert global_v6.endswith(v6_tail)
