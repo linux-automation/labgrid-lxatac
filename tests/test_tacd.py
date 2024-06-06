586fd21b897f3e650e956b190ef73d5fb4cf4199
Chris Fiege <cfi@pengutronix.de>
2024-06-06 15:15:06 +0200

    test/tacd: Add test_tacd_eet_analog
    This test tests multiple analog values using the eet.

--- tests/test_tacd.py
+++ tests/test_tacd.py
@@ -86,3 +86,140 @@ def test_tacd_http_switch_output(strategy, online, control, states):
         time.sleep(0.5)
         res = get_endpoint(strategy.network.address, control)
         assert res.content == state
+
+
+@pytest.mark.lg_feature("eet")
+@pytest.mark.parametrize(
+    "endpoint, link, bounds, precondition",
+    (
+        (
+            "v1/output/out_0/feedback/voltage",
+            "5V_1K -> -5V -> BUS1 -> OUT0",
+            (-5.5, -4.5),
+            ("v1/output/out_0/asserted", b"false"),
+        ),
+        (
+            "v1/output/out_0/feedback/voltage",
+            "5V_1K -> 5V -> BUS1 -> OUT0",
+            (4.5, 5.5),
+            ("v1/output/out_0/asserted", b"false"),
+        ),
+        (
+            "v1/output/out_1/feedback/voltage",
+            "5V_1K -> -5V -> BUS1 -> OUT1",
+            (-5.5, -4.5),
+            ("v1/output/out_1/asserted", b"false"),
+        ),
+        (
+            "v1/output/out_1/feedback/voltage",
+            "5V_1K -> 5V -> BUS1 -> OUT1",
+            (4.5, 5.5),
+            ("v1/output/out_1/asserted", b"false"),
+        ),
+        (
+            "v1/output/out_0/feedback/voltage",
+            "AUX1 -> BUS1 -> OUT0",
+            (3.0, 3.6),
+            ("v1/output/out_0/asserted", b"false"),
+        ),
+        (
+            "v1/output/out_1/feedback/voltage",
+            "AUX1 -> BUS1 -> OUT1",
+            (3.0, 3.6),
+            ("v1/output/out_1/asserted", b"false"),
+        ),
+        (
+            "v1/usb/host/port1/feedback/current",
+            "USB1_IN -> BUS1 -> CURR -> SHUNT_78R",
+            (0.045, 0.065),
+            None,
+        ),
+        (
+            "v1/usb/host/port1/feedback/current",
+            "USB1_IN -> BUS1 -> CURR -> SHUNT_15R",
+            (0.29, 0.33),
+            None,
+        ),
+        (
+            "v1/usb/host/port1/feedback/current",
+            "USB1_IN -> BUS1 -> CURR -> SHUNT_10R, USB1_IN -> BUS1 -> CURR -> SHUNT_15R",
+            (0.46, 0.5),
+            None,
+        ),
+        (
+            "v1/usb/host/port2/feedback/current",
+            "USB2_IN -> BUS1 -> CURR -> SHUNT_78R",
+            (0.045, 0.065),
+            None,
+        ),
+        (
+            "v1/usb/host/port2/feedback/current",
+            "USB2_IN -> BUS1 -> CURR -> SHUNT_15R",
+            (0.29, 0.33),
+            None,
+        ),
+        (
+            "v1/usb/host/port2/feedback/current",
+            "USB2_IN -> BUS1 -> CURR -> SHUNT_10R, USB2_IN -> BUS1 -> CURR -> SHUNT_15R",
+            (0.46, 0.5),
+            None,
+        ),
+        (
+            "v1/usb/host/port3/feedback/current",
+            "USB3_IN -> BUS1 -> CURR -> SHUNT_78R",
+            (0.045, 0.065),
+            None,
+        ),
+        (
+            "v1/usb/host/port3/feedback/current",
+            "USB3_IN -> BUS1 -> CURR -> SHUNT_15R",
+            (0.29, 0.33),
+            None,
+        ),
+        (
+            "v1/usb/host/port3/feedback/current",
+            "USB3_IN -> BUS1 -> CURR -> SHUNT_10R, USB3_IN -> BUS1 -> CURR -> SHUNT_15R",
+            (0.46, 0.5),
+            None,
+        ),
+        (
+            "v1/dut/feedback/voltage",
+            "AUX3 -> BUS1 -> PWR_IN",
+            (11.5, 12.5),
+            ("v1/dut/powered", b'"On"'),
+        ),
+        (
+            "v1/dut/feedback/current",
+            "AUX3 -> BUS1 -> PWR_IN",
+            (-0.05, 0.05),
+            ("v1/dut/powered", b'"On"'),
+        ),
+        (
+            "v1/dut/feedback/current",
+            "AUX3 -> BUS1 -> PWR_IN, PWR_OUT -> BUS2 -> CURR -> SHUNT_78R",
+            (0.14, 0.16),
+            ("v1/dut/powered", b'"On"'),
+        ),
+        (
+            "v1/dut/feedback/current",
+            "AUX3 -> BUS1 -> PWR_IN, PWR_OUT -> BUS2 -> CURR -> SHUNT_15R",
+            (0.70, 0.85),
+            ("v1/dut/powered", b'"On"'),
+        ),
+        (
+            "v1/dut/feedback/current",
+            "AUX3 -> BUS1 -> PWR_IN, PWR_OUT -> BUS2 -> CURR -> SHUNT_10R",
+            (1.1, 1.2),
+            ("v1/dut/powered", b'"On"'),
+        ),
+    ),
+)
+def test_tacd_eet_analog(strategy, online, endpoint, link, bounds, precondition):
+    """Test if analog measurements work with values not equal to zero."""
+    if precondition:
+        put_endpoint(strategy.network.address, precondition[0], precondition[1])
+
+    strategy.eet.link(link)  # connect supply to output
+    time.sleep(0.2)  # give the analog world a moment to settle
+    res = get_json_endpoint(strategy.network.address, endpoint)
+    assert bounds[0] <= res["value"] <= bounds[1]
