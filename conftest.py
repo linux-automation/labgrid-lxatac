2448bcddfb77f5b6ec4b7672bf143b1cc4985b86
Chris Fiege <cfi@pengutronix.de>
2024-06-07 11:10:11 +0200

    conftest: Remove redundant target
    The strategy already has all drivers in it's bindings. So there is no
    need for the additional ´target´ in these fixtures.

--- conftest.py
+++ conftest.py
@@ -4,29 +4,29 @@ import pytest
 
 
 @pytest.fixture(scope="function")
-def barebox(strategy, target):
+def barebox(strategy):
     try:
         strategy.transition("barebox")
     except Exception as e:
         traceback.print_exc()
         pytest.exit(f"Transition into barebox failed: {e}", returncode=3)
 
-    return target.get_driver("BareboxDriver")
+    return strategy.barebox
 
 
 @pytest.fixture(scope="function")
-def shell(strategy, target):
+def shell(strategy):
     try:
         strategy.transition("shell")
     except Exception as e:
         traceback.print_exc()
         pytest.exit(f"Transition into shell failed: {e}", returncode=3)
 
-    return target.get_driver("ShellDriver")
+    return strategy.shell
 
 
 @pytest.fixture(scope="function")
-def online(strategy, target):
+def online(strategy):
     try:
         strategy.transition("network")
     except Exception as e:
@@ -35,25 +35,25 @@ def online(strategy, target):
 
 
 @pytest.fixture(scope="function")
-def system0_shell(strategy, target):
+def system0_shell(strategy):
     try:
         strategy.transition("system0")
     except Exception as e:
         traceback.print_exc()
         pytest.exit(f"Transition into system0 shell failed: {e}", returncode=3)
 
-    return target.get_driver("ShellDriver")
+    return strategy.shell
 
 
 @pytest.fixture(scope="function")
-def system1_shell(strategy, target):
+def system1_shell(strategy):
     try:
         strategy.transition("system1")
     except Exception as e:
         traceback.print_exc()
         pytest.exit(f"Transition into system1 shell failed: {e}", returncode=3)
 
-    return target.get_driver("ShellDriver")
+    return strategy.shell
 
 
 def pytest_configure(config):
