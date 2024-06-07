1d1b1f3df2002eba9ee788069c3a3cdedd6fcdef
Chris Fiege <cfi@pengutronix.de>
2024-06-07 13:58:11 +0200

    tests: Add tests for Linux random number generators

---
+++ tests/test_linux_rng.py
@@ -0,0 +1,22 @@
+"""Run linux random number generator checks."""
+
+
+def test_dev_random(shell):
+    """Test if we can read /dev/random and not get timeout"""
+    shell.run_check("timeout 5 dd if=/dev/random of=/dev/null bs=1 count=16")
+
+
+def test_dev_urandom(shell):
+    """Test if we can read /dev/urandom"""
+    shell.run_check("timeout 5 dd if=/dev/urandom of=/dev/null bs=1 count=16")
+
+
+def test_dev_hwrng(shell):
+    """Test if we can read /dev/hwrng"""
+    shell.run_check("timeout 5 dd if=/dev/hwrng of=/dev/null bs=1 count=16")
+
+
+def test_hwrng_task(shell):
+    """Test that hwrng kernel task is running"""
+    _, _, exit = shell.run("pgrep -P 2 -x hwrng")
+    assert exit == 0
