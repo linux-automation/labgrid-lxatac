8a495ef49fe7bfa2a782ddb5a696f0f21f01ea06
Fabian Pfitzner <f.pfitzner@pengutronix.de>
2024-07-02 12:15:05 +0200

    tests: network connectivity

--- tests/test_network.py
+++ tests/test_network.py
@@ -99,3 +99,38 @@ def test_network_performance(prepare_network, shell, bandwidth, expected):
     # Reset bandwidth configuration
     shell.run("ethtool -s uplink speed 1000")
     shell.run("ip netns exec dut-namespace ethtool -s dut speed 1000")
+
+
+def test_network_connectivity(shell):
+    """Test connectivity by sending a ping to the gateway"""
+
+    bridge_iface = "tac-bridge"
+    link_state = shell.run_check(f"cat /sys/class/net/{bridge_iface}/operstate")[0]
+    assert link_state[0] == "up", f"link_state is {link_state}"
+
+    shell.run_check(f"ping -I {bridge_iface} -c1 _gateway")
+
+
+def test_network_interfaces(shell):
+    """Test whether all expected network interfaces are present"""
+
+    found_interfaces = set()
+    stdout = shell.run_check("ip link show")
+
+    expected_interfaces = {
+        "lo",
+        "can0_iobus",
+        "can1",
+        "switch",
+        "dut@switch",
+        "uplink@switch",
+        "tac-bridge",
+    }
+
+    for line in stdout:
+        if line[0] in (" ", "\t"):
+            continue
+
+        found_interfaces.add(line.split(":")[1].strip())
+
+    assert expected_interfaces == found_interfaces
