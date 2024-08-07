644152dd95158b24fd2a5a6327f7be9489535e2a
Chris Fiege <cfi@pengutronix.de>
2024-08-07 09:47:26 +0200

    tests/linux_stress: Do not check for limits, but record results
    stress-ng's metric `bogo-ops-per-second-usr-sys-time` seems to be too
    unstable to test against.
    Thus for now, we do not test against actual limits.
    
    Instead we collect these metrics in our test results and test for a
    successful run of `stress-ng` itself.

--- tests/test_linux_stress.py
+++ tests/test_linux_stress.py
@@ -10,33 +10,26 @@ def run_stressor(shell, args):
     return data
 
 
-# run a single test
-def test_stress_matrix(shell):
-    data = run_stressor(shell, "--matrix 0")
-    metrics = data["metrics"][0]
-    assert metrics["stressor"] == "matrix"
-    assert metrics["bogo-ops-per-second-usr-sys-time"] == pytest.approx(128, rel=0.1)  # ±10%
-
-
 # run multiple similar tests
 @pytest.mark.parametrize(
-    "stressor, expected",
+    "stressor",
     (
-        ("zero", pytest.approx(170496, rel=0.1)),  # ±10%
-        ("yield", pytest.approx(109129, rel=0.1)),  # ±10%
-        ("switch", pytest.approx(25591, rel=0.2)),  # ±20%
-        ("shm", pytest.approx(61, rel=0.1)),  # ±10%
-        ("remap", pytest.approx(4.8, rel=0.1)),  # ±10%
-        ("qsort", pytest.approx(0.98, rel=0.1)),  # ±10%
-        ("bsearch", pytest.approx(16.3, rel=0.1)),  # ±10%
-        ("atomic", pytest.approx(62, rel=0.1)),  # ±10%
+        "matrix",
+        "zero",
+        "yield",
+        "switch",
+        "shm",
+        "remap",
+        "qsort",
+        "bsearch",
+        "atomic",
     ),
 )
-def test_stress(shell, stressor, expected):
+def test_stress(shell, stressor, record_property):
     args = "--shm-bytes 1M" if stressor == "shm" else ""
 
     data = run_stressor(shell, f"--{stressor} 0 {args}")
     metrics = data["metrics"][0]
-
     assert metrics["stressor"] == stressor
-    assert metrics["bogo-ops-per-second-usr-sys-time"] == expected
+
+    record_property("bogo-ops-per-second-usr-sys-time", metrics["bogo-ops-per-second-usr-sys-time"])
