4c82e450d8731303ef4e814a9d0c24980203ebb4
Chris Fiege <cfi@pengutronix.de>
2024-09-10 13:09:27 +0200

    test_filesystem_sizes: Use findmnt --json instead of df
    Using `findmnt --json` we can directly parse the tools output.
    This is considered better practice than parsing the shell output of
    `df`.

--- tests/test_filesystems.py
+++ tests/test_filesystems.py
@@ -8,34 +8,30 @@ GIGA = 1_000 * MEGA
 
 
 def filesystem_sizes(shell):
-    # These tests are not RAUC tests per se, but we want to run them for both
-    # slots and jamming them in between the RAUC tests using e.g., dependencies
-    # did not work to well / was even messier than just placing them here.
-
-    # $ df -B1
-    # Filesystem      1B-blocks      Used Available Use% Mounted on
-    # /dev/root      1052303360 832401408 145055744  86% /
-    # ...
-    df = shell.run_check("df -B1")
-
-    # [{"Filesystem": "/dev/root", "1B-blocks": ...}, {"Filesystem": ...
-    df = list(dict(zip(df[0].split(), line.split())) for line in df[1:])
-
-    # {'/': {'Filesystem': '/dev/root', ...
-    df = dict((e["Mounted"], e) for e in df)
-
     # / should have some spare space available
-    assert int(df["/"]["1B-blocks"]) in range(1_900 * MEGA, 2_500 * MEGA)
-    assert int(df["/"]["Used"]) / int(df["/"]["1B-blocks"]) < 0.6
+    stdout = shell.run_check("findmnt -b --json -o SIZE,USED /")
+    fs_info = json.loads("".join(stdout))["filesystems"][0]
+    assert fs_info["size"] in range(1_900 * MEGA, 2_500 * MEGA)
+    assert fs_info["used"] / fs_info["size"] < 0.6
 
     # /srv should be mostly empty
-    assert int(df["/srv"]["1B-blocks"]) in range(8 * GIGA, 16 * GIGA)
-    assert int(df["/srv"]["Used"]) / int(df["/srv"]["1B-blocks"]) < 0.1
+    stdout = shell.run_check("findmnt -b --json -o SIZE,USED /srv")
+    fs_info = json.loads("".join(stdout))["filesystems"][0]
+    assert fs_info["size"] in range(8 * GIGA, 16 * GIGA)
+    assert fs_info["used"] / fs_info["size"] < 0.1
 
     # /run, /tmp, /var/volatile should be mostly empty
-    assert int(df["/run"]["Used"]) / int(df["/run"]["1B-blocks"]) < 0.2
-    assert int(df["/tmp"]["Used"]) / int(df["/tmp"]["1B-blocks"]) < 0.2
-    assert int(df["/var/volatile"]["Used"]) / int(df["/var/volatile"]["1B-blocks"]) < 0.2
+    stdout = shell.run_check("findmnt -b --json -o SIZE,USED /run")
+    fs_info = json.loads("".join(stdout))["filesystems"][0]
+    assert fs_info["used"] / fs_info["size"] < 0.2
+
+    stdout = shell.run_check("findmnt -b --json -o SIZE,USED /tmp")
+    fs_info = json.loads("".join(stdout))["filesystems"][0]
+    assert fs_info["used"] / fs_info["size"] < 0.2
+
+    stdout = shell.run_check("findmnt -b --json -o SIZE,USED /var/volatile")
+    fs_info = json.loads("".join(stdout))["filesystems"][0]
+    assert fs_info["used"] / fs_info["size"] < 0.2
 
 
 def test_partition_sizes(shell):
