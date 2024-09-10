ec66cb2e13cedc2f4248e97a3337cb2116964823
Chris Fiege <cfi@pengutronix.de>
2024-09-10 15:08:32 +0200

    test_linux_hardware: Test for I2C devices instead of the subsystem
    The previous test very bluntly only tested if the I2C-Subsystem in the
    kernel has been loaded at all. But this does not tell us anything about
    the actual devices on the bus we want to use.
    
    The new tests instead try to use the I2C devices the way userspace
    would.
    Actually none of the I2C devices are directly used by userspace:
    
    * The EEPROMs are read by the bootloader and their information
      are added to the devicetree's chosen_node.
      But for the EEPROMs we can at least do a read, so we can check
      if this works.
    * The PMIC is used inside the kernel.
      So we will only check the hardware has been probed.
    * The USB-Hub is also used inside the kernel
      So we will only check the hardware has been probed.

--- tests/test_linux_hardware.py
+++ tests/test_linux_hardware.py
@@ -1,9 +1,41 @@
 import json
 
 
-def test_linux_i2c_bus(shell):
-    """Test if the i2c subsystem exists"""
-    shell.run_check("test -d /sys/bus/i2c")
+def test_linux_i2c_bus_0_eeprom(shell):
+    """
+    Test if the config EEPROM on i2cbus-0 can be read.
+    """
+
+    # the first 4 bytes of the EEPROM will always contain the magic for the TLV data stored in there
+    magic = shell.run_check("dd if=/sys/bus/i2c/devices/0-0050/eeprom bs=1 count=4 status=none | base64")[0]
+    assert magic == "xolcIQ=="
+
+
+def test_linux_i2c_bus_1_usbhub(shell):
+    """
+    Test if a driver for the USB hub on i2cbus-1 has been loaded.
+    """
+
+    name = shell.run_check("cat /sys/bus/i2c/devices/i2c-1/1-002c/name")[0]
+    assert name == "usb2514b"
+
+
+def test_linux_i2c_bus_2_eeprom(shell):
+    """
+    Test if the config EEPROM on i2cbus-2 can be read.
+    """
+
+    # the first 4 bytes of the EEPROM will always contain the magic for the TLV data stored in there
+    magic = shell.run_check("dd if=/sys/bus/i2c/devices/2-0050/eeprom bs=1 count=4 status=none | base64")[0]
+    assert magic == "vCiN/g=="
+
+
+def test_linux_i2c_bus_2_pmic(shell):
+    """
+    Test if a driver for the PMIC on i2cbus-2 has been loaded.
+    """
+    name = shell.run_check("cat /sys/bus/i2c/devices/i2c-2/2-0033/name")[0]
+    assert name == "stpmic1"
 
 
 def test_linux_spi_bus(shell):
