f1b6f1a35ca25d591740c0f36d4ca44a1d0c2c41
Chris Fiege <cfi@pengutronix.de>
2024-09-11 15:30:46 +0200

    test_linux_clock: Simplify format handling
    The original test case contained a lot of code to handle different
    format strings. But it's unclear why that all that complexity was
    actually needed.
    
    This change removes all the complex format handling and reduces it to a
    bare minimum for this DUT.

--- tests/test_linux_clock.py
+++ tests/test_linux_clock.py
@@ -4,38 +4,6 @@ from time import monotonic
 import pytest
 
 
-def strptime(s):
-    fmts = [
-        # 'Fri Jan  1 05:12:45 UTC 2016'
-        "%a %b  %d %X UTC %Y",
-        # 'Thu Jan  1 05:13:44 1970  .087782 seconds'
-        "%a %b  %d %X %Y  .%f seconds",
-        # 'Thu Jan  1 05:13:44 1970  0.087782 seconds'
-        "%a %b  %d %X %Y  0.%f seconds",
-        # '1970-01-01 00:13:32+00:00'
-        "%Y-%m-%d %H:%M:%S+00:00",
-        # '1970-01-01 00:13:32.135780+00:00'
-        "%Y-%m-%d %H:%M:%S.%f+00:00",
-    ]
-
-    for fmt in fmts:
-        try:
-            return datetime.strptime(s, fmt)
-        except ValueError:
-            pass
-    raise ValueError(f"time data '{s}' does not match known formats")
-
-
-def get_date_fmt(shell):
-    "Check if the installed date support %6N"
-    stdout, _, rc = shell.run("date -u +%6N")
-    assert rc == 0
-    if "N" in stdout[0]:
-        return "'%Y-%m-%d %H:%M:%S+00:00'"
-    else:
-        return "'%Y-%m-%d %H:%M:%S.%6N+00:00'"
-
-
 def test_clock_system(shell):
     """
     This test checks the system clock frequency by running `date` with `sleep`
@@ -45,23 +13,21 @@ def test_clock_system(shell):
     def run_cmd(sleep):
         host_start_time = monotonic()
 
+        date_fmt = "'%Y-%m-%d %H:%M:%S.%N'"
         stdout, _, rc = shell.run(
             f"date -u +{date_fmt}; sleep {sleep}; date -u +{date_fmt}", timeout=float(sleep) + 5.0
         )
 
         assert rc == 0
 
-        date0_str = stdout[0]
-        date1_str = stdout[1]
-
-        date0 = strptime(date0_str)
-        date1 = strptime(date1_str)
+        # `date` on the DUT supports %N (nanoseconds), `datetime` can only parse %f (microseconds).
+        # `date` adds zero-padding to all fields, so we can simply drop the last three digits of %N.
+        date0 = datetime.strptime(stdout[0][:-3], "%Y-%m-%d %H:%M:%S.%f")
+        date1 = datetime.strptime(stdout[1][:-3], "%Y-%m-%d %H:%M:%S.%f")
 
         assert date0 <= date1
 
         return (monotonic() - host_start_time), (date1 - date0).total_seconds()
-
-    date_fmt = get_date_fmt(shell)
 
     # get connection offset
     connection_offset, _ = run_cmd(0)
