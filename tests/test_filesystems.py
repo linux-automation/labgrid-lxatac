6875776f9140f8033b484710faac82201c7767be
Chris Fiege <cfi@pengutronix.de>
2024-09-12 17:52:41 +0200

    test_filesystems: Only test in base system
    Until now we have tested the size of the file systems in `slot 0` and
    `slot 1`. This seems natural, since these are two independent systems.
    
    But both slots basically contain the same system - only installed via
    two different methods.
    We currently have no reason to believe that the system installed via
    RAUC may have another file system layout that the system installed via
    fastboot.
    
    Thus we rewrite the tests to only test the file system sizes in `slot 0`
    and remove a good amount of complexity and testing time.
    If we ever encounter problems in this realm we can still write tests
    that focus on the actual problems fixed.

--- tests/test_filesystems.py
+++ tests/test_filesystems.py
@@ -7,7 +7,25 @@ MEGA = 1_000 * KILO
 GIGA = 1_000 * MEGA
 
 
-def filesystem_sizes(shell):
+def test_partition_sizes(shell):
+    stdout = shell.run_check("lsblk -b --json /dev/mmcblk1")
+    json_info = json.loads("".join(stdout))
+
+    part_sizes = {child["name"]: child["size"] for child in json_info["blockdevices"][0]["children"]}
+
+    assert part_sizes["mmcblk1p1"] in range(2_000 * MEGA, 2_500 * MEGA)
+    assert part_sizes["mmcblk1p2"] in range(2_000 * MEGA, 2_500 * MEGA)
+    assert part_sizes["mmcblk1p3"] in range(8 * GIGA, 16 * GIGA)
+
+
+@pytest.mark.xfail(
+    reason="There is a known bug, in which directories are created in `/srv` during the first boot. "
+    "If this happens `/srv` will not be mounted with the correct partition on consecutive boots in this slot. "
+    "Since we have r/w root-partition most of the system behaves as expected. "
+    "But changes to /srv are lost on update. "
+    "This behavior needs to be fixed - but is currently expected to fail."
+)
+def test_filesystem_sizes(shell):
     # / should have some spare space available
     stdout = shell.run_check("findmnt -b --json -o SIZE,USED /")
     fs_info = json.loads("".join(stdout))["filesystems"][0]
@@ -32,31 +50,3 @@ def filesystem_sizes(shell):
     stdout = shell.run_check("findmnt -b --json -o SIZE,USED /var/volatile")
     fs_info = json.loads("".join(stdout))["filesystems"][0]
     assert fs_info["used"] / fs_info["size"] < 0.2
-
-
-def test_partition_sizes(shell):
-    stdout = shell.run_check("lsblk -b --json /dev/mmcblk1")
-    json_info = json.loads("".join(stdout))
-
-    part_sizes = {child["name"]: child["size"] for child in json_info["blockdevices"][0]["children"]}
-
-    assert part_sizes["mmcblk1p1"] in range(2_000 * MEGA, 2_500 * MEGA)
-    assert part_sizes["mmcblk1p2"] in range(2_000 * MEGA, 2_500 * MEGA)
-    assert part_sizes["mmcblk1p3"] in range(8 * GIGA, 16 * GIGA)
-
-
-@pytest.mark.xfail(
-    reason="There is a known bug, in which directories are created in `/srv` during the first boot. "
-    "If this happens `/srv` will not be mounted with the correct partition on consecutive boots in this slot. "
-    "Since we have r/w root-partition most of the system behaves as expected. "
-    "But changes to /srv are lost on update. "
-    "This behavior needs to be fixed - but is currently expected to fail."
-)
-@pytest.mark.slow
-def test_system0_filesystem_sizes(system0_shell):
-    filesystem_sizes(system0_shell)
-
-
-@pytest.mark.slow
-def test_system1_filesystem_sizes(system1_shell):
-    filesystem_sizes(system1_shell)
