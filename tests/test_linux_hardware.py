5e9b9bf0b0187372af0baeef9a5710f882fbd7b1
Leonard Göhrs <l.goehrs@pengutronix.de>
2024-11-13 13:42:06 +0100

    test_hardware: sort the test cases
    Before the bus re-numbering the tests were nicely ordered.
    
    Bring that back.
    
    Signed-off-by: Leonard Göhrs <l.goehrs@pengutronix.de>

--- tests/test_linux_hardware.py
+++ tests/test_linux_hardware.py
@@ -11,15 +11,6 @@ def test_linux_i2c_bus_0_eeprom(shell):
     assert magic == "xolcIQ=="
 
 
-def test_linux_i2c_bus_2_usbhub(shell):
-    """
-    Test if a driver for the USB hub on i2cbus-2 has been loaded.
-    """
-
-    [name] = shell.run_check("cat /sys/bus/i2c/devices/2-002c/name")
-    assert name == "usb2514b"
-
-
 def test_linux_i2c_bus_1_eeprom(shell):
     """
     Test if the config EEPROM on i2cbus-1 can be read.
@@ -38,16 +29,13 @@ def test_linux_i2c_bus_1_pmic(shell):
     assert name == "stpmic1"
 
 
-def test_linux_spi_2_ethernet_switch(shell):
+def test_linux_i2c_bus_2_usbhub(shell):
     """
-    Test if the Ethernet switch on spi-2 works.
+    Test if a driver for the USB hub on i2cbus-2 has been loaded.
     """
-    shell.run_check("test -d /sys/bus/spi/devices/spi2.0")
 
-    # Statistics for the uplink interface are read from the Ethernet switch using spi0.
-    # So a non-zero value will indicate that the device on the bus works.
-    [rx_packets] = shell.run_check("ethtool -S uplink  | grep rx_packets")
-    assert int(rx_packets.split(":")[-1]) > 0
+    [name] = shell.run_check("cat /sys/bus/i2c/devices/2-002c/name")
+    assert name == "usb2514b"
 
 
 def test_linux_spi_0_adc(shell):
@@ -71,6 +59,18 @@ def test_linux_spi_1_lcd(shell):
     # But this way, we at least know that the correct drm device has been probed.
     [name] = shell.run_check("cat /sys/bus/spi/devices/spi1.0/graphics/fb0/name")
     assert name == "panel-mipi-dbid"
+
+
+def test_linux_spi_2_ethernet_switch(shell):
+    """
+    Test if the Ethernet switch on spi-2 works.
+    """
+    shell.run_check("test -d /sys/bus/spi/devices/spi2.0")
+
+    # Statistics for the uplink interface are read from the Ethernet switch using spi0.
+    # So a non-zero value will indicate that the device on the bus works.
+    [rx_packets] = shell.run_check("ethtool -S uplink  | grep rx_packets")
+    assert int(rx_packets.split(":")[-1]) > 0
 
 
 def test_sensors(shell):
