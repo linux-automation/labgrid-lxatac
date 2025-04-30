673c33cdf74df951e51f13a75da58d16e5bc4f09
Chris Fiege <cfi@pengutronix.de>
2025-04-30 12:47:47 +0200

    test/linux: Test if kernel logs only known warn and errors
    We know that out kernel logs some warnings and errors on startup.
    These are accepted.
    With this test we make sure that the kernel only logs these messages and
    nothing new.
    
    Co-authored-by: Leonard Göhrs <leonard@goehrs.eu>

--- tests/test_linux_pstore.py
+++ tests/test_linux_pstore.py
@@ -1,6 +1,35 @@
+import re
+
+
 def test_pstore_fs(shell):
     """
     Test if the pstore filesystem exists.
     """
 
     shell.run_check("test -d /sys/fs/pstore")
+
+
+def test_kernel_messages(shell, check):
+    """
+    Test if the kernel only logs messages that we expect.
+
+    This test will ignore some harmless messages that can happen during normal operation.
+    """
+
+    expected = {
+        "spi_stm32 44009000.spi: failed to request tx dma channel",
+        "spi_stm32 44009000.spi: failed to request rx dma channel",
+        "clk: failed to reparent ethck_k to pll4_p: -22",
+        "stm32-dwmac 5800a000.ethernet switch: Adding VLAN ID 0 is not supported",
+    }
+
+    allowed = {
+        # The following messages can happen during other tests and are harmless
+        "sd 0:0:0:0: [sda] No Caching mode page found",
+        "sd 0:0:0:0: [sda] Assuming drive cache: write through",
+    }
+
+    messages = shell.run_check("dmesg -l warn -l err -l crit -l alert -l emerg -k")
+    messages = set(re.sub(r"^\[\s*\d+\.\d+\] ", "", m) for m in messages)
+
+    assert messages - allowed == expected
